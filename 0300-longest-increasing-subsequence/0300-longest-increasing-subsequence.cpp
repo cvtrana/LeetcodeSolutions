831fb3c6527a7200fee1d269e114ed8270df9a7b
Nikhil Rana <97181423+cvtrana@users.noreply.github.com>
2023-04-29 23:26:31 +0530

    Time: 326 ms (55.57%), Space: 10.2 MB (99.14%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int lengthOfLIS(vector<int>& nums) {
-        dp.resize(size(nums), vector<int>(1+size(nums), -1));   // dp[i][j] denotes max LIS starting from i when nums[j] is previous picked element
-        return solve(nums, 0, -1);
+    int dp[2501];
+    int rec(int level,vector<int> &nums){
+        if(level < 0) {
+            return 0;
+        }
+        
+        if(dp[level]!=-1)return dp[level];
+        
+        int ans =1;
+        for(int i=0;i<level;i++){
+            if(nums[i] < nums[level]){
+                ans =  max(ans,1 + rec(i,nums));
+            }
+        }
+        return dp[level] = ans;
     }
-    int solve(vector<int>& nums, int i, int prev_i) {
-        if(i >= size(nums)) return 0;
-        if(dp[i][prev_i+1] != -1) return dp[i][prev_i+1];
-        int take = 0, dontTake = solve(nums, i + 1, prev_i);
-        if(prev_i == -1 || nums[i] > nums[prev_i]) take = 1 + solve(nums, i + 1, i); // try picking current element if no previous element is chosen or current > nums[prev_i]
-        return dp[i][prev_i+1] = max(take, dontTake);
+    int lengthOfLIS(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        int best =0;
+        for(int i=0;i<nums.size();i++){
+            best = max(best,rec(i,nums));
+            
+            
+        }
+        return best;
+        
     }
 };
