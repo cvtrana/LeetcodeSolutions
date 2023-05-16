ebba1954089948ac9217a3ca9b2dba3cd9a0750f
Nikhil Rana <97181423+cvtrana@users.noreply.github.com>
2023-05-16 23:56:13 +0530

    Time: 370 ms (29.01%), Space: 49.4 MB (26.43%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();  
+        sort(nums.begin() , nums.end());  // sort the array to use the two pointers method
+        vector<vector<int>> ans;  
+        set<vector<int>> store;   
+		
+        for(int i = 0 ; i < n; i++){
+		
+            for(int j = i + 1; j < n ; j++){
+                long long res = (nums[i] +  nums[j]);
+			
+                long long new_target  =  target - res;
+                
+                int x = j+1 , y = n-1;
+                
+                while(x < y){
+				
+                    long long  sum = nums[x] + nums[y];
+                    
+                    if(sum > new_target) y--;
+                    else if(sum < new_target ) x++;
+                    else  {
+                        store.insert({nums[i] , nums[j] , nums[x] , nums[y]});
+                        x++;
+                        y--;
+                    };
+                }
+            }
+        }
+		
+        for(auto i : store){
+            ans.push_back(i);  // store the answers in an array(ans)
+        }
+		
+        return ans;
+    }
+};
