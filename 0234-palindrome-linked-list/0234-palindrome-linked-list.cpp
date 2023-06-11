ba28f04ea8ad82d794b5d08116606eef6b8da844
Nikhil Rana <97181423+cvtrana@users.noreply.github.com>
2023-06-11 16:45:59 +0530

    Time: 270 ms (42.15%), Space: 118.1 MB (52.93%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,24 +10,66 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        ListNode* temp =  head;
-        vector<int> check;
-        while(temp!=NULL){
-            int d = temp->val;
-            temp = temp->next;
-            check.push_back(d);
+    ListNode* reversed(ListNode* &head){
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        
+        while(curr != NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
-        int i =0;
-        int j = check.size()-1;
-        while(i<=j){
-            if(check[i]!=check[j]){
+        return prev; // idhr head node hogi
+        
+        
+        
+        
+        
+    }
+    
+    ListNode* getmid(ListNode* &head){
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        
+        while(fast!=NULL && fast->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+    
+    bool isPalindrome(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return true;
+        }
+        
+        // step 1 : getMiddle
+        ListNode* middle = getmid(head);
+        ListNode* temp = middle->next;
+        
+        
+        // reverse the second half
+        middle->next = reversed(temp);
+        
+        
+        // compare both the half
+        
+        ListNode* head1 = head;
+        ListNode* head2 = middle->next;
+        while(head2!=NULL){
+            
+            if(head1->val != head2->val){
                 return false;
+                 
             }
-            i++;
-            j--;
+            head2 = head2->next;
+            head1 = head1->next;
         }
         return true;
+        
+        
         
     }
 };
