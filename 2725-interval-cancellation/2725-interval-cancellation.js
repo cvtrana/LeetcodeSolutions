6eaeacaea01b6955793cc8b760e6068711bb85ea
Nikhil Rana <97181423+cvtrana@users.noreply.github.com>
2023-06-16 14:29:26 +0530

    Time: 82 ms (11.20%), Space: 42.3 MB (32.91%) - LeetHub

---
+++ 2725-interval-cancellation/2725-interval-cancellation.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+  var timerId;
+  var time = 0;
+  var result = [];
+
+  function executeFn() {
+    var returnedValue = fn.apply(null, args);
+    result.push({ time: time, returned: returnedValue });
+    time += t;
+  }
+
+  function cancelFn() {
+    clearInterval(timerId);
+  }
+
+  executeFn();
+  timerId = setInterval(executeFn, t);
+
+  return cancelFn;
+};
