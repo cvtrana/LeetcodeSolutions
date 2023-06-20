aa16a6d876d10d9317afb2f5898d96971d576e70
Nikhil Rana <97181423+cvtrana@users.noreply.github.com>
2023-06-20 16:57:49 +0530

    Time: 25 ms (63.30%), Space: 16.4 MB (32.71%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,20 +1,19 @@
 class MinStack {
-private:
-    stack<int> s1;
-    stack<int> s2;
 public:
-    void push(int x) {
-	    s1.push(x);
-	    if (s2.empty() || x <= getMin())  s2.push(x);	    
+    vector< pair<int,int> > s;
+	
+    MinStack() { }
+    
+    void push(int val) {
+        if(s.empty())
+            s.push_back({val,val});
+        else
+            s.push_back({val,min(s.back().second,val)});    
     }
-    void pop() {
-	    if (s1.top() == getMin())  s2.pop();
-	    s1.pop();
-    }
-    int top() {
-	    return s1.top();
-    }
-    int getMin() {
-	    return s2.top();
-    }
+    
+    void pop() { s.pop_back(); }
+    
+    int top() { return s.back().first; }
+    
+    int getMin() { return s.back().second; }
 };
