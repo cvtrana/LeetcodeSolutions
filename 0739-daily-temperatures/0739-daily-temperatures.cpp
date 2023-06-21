f2da4bbafc0fb03478af3498c830d77190816908
Nikhil Rana <97181423+cvtrana@users.noreply.github.com>
2023-06-21 10:26:53 +0530

    Time: 217 ms (75.31%), Space: 98.7 MB (75.01%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n,0);
+        // can be implemented using binary search
+        // concept used :  next greater element
+        stack<int> st;
+        
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && temperatures[st.top()] <= temperatures[i]){
+                st.pop();
+            }
+            
+            if(!st.empty()){
+                ans[i] = st.top()-i;
+            }
+            
+            st.push(i);
+            
+        }
+        
+        return ans;
+        
+    }
+};
