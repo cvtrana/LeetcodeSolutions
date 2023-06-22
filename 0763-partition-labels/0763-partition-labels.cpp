29541ac8852554ebcd9d113c38dd88dde41a39de
Nikhil Rana <97181423+cvtrana@users.noreply.github.com>
2023-06-22 23:24:30 +0530

    Time: 6 ms (47.11%), Space: 6.6 MB (86.72%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -1,12 +1,33 @@
 class Solution {
 public:
-    vector<int> partitionLabels(string S) {
-  vector<int> res, pos(26, 0);  
-  for (auto i = 0; i < S.size(); ++i) pos[S[i] - 'a'] = i;
-  for (auto i = 0, idx = INT_MIN, last_i = 0; i < S.size(); ++i) {
-    idx = max(idx, pos[S[i] - 'a']);
-    if (idx == i) res.push_back(i - exchange(last_i, i + 1) + 1);
-  }
-  return res;
-}
+    vector<int> partitionLabels(string s) {
+		// vector for keeping the track of  last occurence of every character
+        vector<int> end_idx(26,0);
+        
+        for(int i = 0; i < s.length(); ++i)
+            end_idx[s[i] - 'a']  = i;
+        
+        vector<int> res;
+        
+        int start = 0, end = 0;
+		// scanning string character by character
+        for(int i = 0; i < s.length(); ++i)
+        {
+		// whenever we get an character we check,
+		// last index of that character
+            end = max(end, end_idx[s[i] - 'a']);
+            
+			// when current i.e i == end
+			// add it to result
+            if( i == end)
+            {
+                // all the characters of current partition included
+                res.push_back(i - start + 1);
+				// update the start pointer for fresh start
+                start = i + 1;
+            }
+        }
+        
+        return res;
+    }
 };
