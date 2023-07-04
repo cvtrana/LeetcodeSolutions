2f524bb4aebe27e8d1759d2488f3fd0c7ffdd54a
Nikhil Rana <97181423+cvtrana@users.noreply.github.com>
2023-07-05 00:19:23 +0530

    Time: 26 ms (99.29%), Space: 7.4 MB (83.26%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -26,7 +26,7 @@ public:
                         dp[i][j] = 2 + dp[i+1][j-1];
                     }
                     else{
-                        dp[i][j] = max(dp[i][j-1],dp[i+1][j]);
+                        dp[i][j] = max(dp[i][j-1],dp[i+1][j]); // check prefixes
                     }
                 }
                 
@@ -36,7 +36,7 @@ public:
             }
             
         }
-        return s.length() - dp[0][s.length()-1];
+        return s.length() - dp[0][s.length()-1]; // total length  - LPS
         
     }
 };
