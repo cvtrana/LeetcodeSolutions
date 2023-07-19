d2cf990512056a85d088d53d2ed1f464fc90306c
Nikhil Rana <97181423+cvtrana@users.noreply.github.com>
2023-07-19 11:15:09 +0530

    Time: 754 ms (5.03%), Space: 116 MB (5.14%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,21 +1,21 @@
-
 class Solution {
-    public:
+public:
+    int get_intervals(vector<vector<int>>& intervals,int i, vector<int>&dp){
+        if(i>=intervals.size()) return 0;
+        if(dp[i] != -1) return dp[i];
+
+        vector<int>temp = {intervals[i][1],INT_MIN};
+        int it = upper_bound(intervals.begin(),intervals.end(),temp) - intervals.begin();
+        int include = 1 + get_intervals(intervals,it,dp);;
+
+        int exclude = get_intervals(intervals,i+1,dp);
+           
+        return dp[i] = max(include,exclude);
+    } 
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        
-        int ans=-1;
-        sort(intervals.begin(),intervals.end(),[&](auto const& a, auto const & b){
-            return a[1] < b[1];
-        });
-        
-        vector<int> prev= intervals[0];
-        for(vector<int> interval: intervals) {
-            if(interval[0] < prev[1])
-                ans++;
-            else
-                prev = interval;
-        }
-        return ans;
+        int n = intervals.size();
+        sort(intervals.begin(),intervals.end());
+        vector<int>dp(n+1,-1);
+        return n - get_intervals(intervals,0,dp) ;
     }
 };
-
